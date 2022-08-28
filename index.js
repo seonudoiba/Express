a95fd825af4ed54ea08a11bef70f776ce1851278
seonudoiba <abiodunigbehinadun17@gmail.com>
2022-08-29 00:37:43 +0100

    First Commit on Express

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import express from 'express';
+const express = require('express');
+const path = require('path')
 
 
 const app = express();
 const PORT = process.env.PORT || 5000
+app.use(express.static(path.join(__dirname, 'basco-html')))
 
-app.get('/', (req, res) => {
-    res.send('<h2>Hello Express</h2>')
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'basco-html'))
+// })
 
 app.listen(PORT, ()=>console.log('listening on port '+ PORT))
